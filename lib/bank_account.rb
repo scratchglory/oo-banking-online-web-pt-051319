093519974dfb112047eab89671374a40377c6089
Adella <adellakenn@gmail.com>
2019-06-08 17:21:48 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,51 @@
-class BankAccount
+=begin
+    BankAccount
+  #initialize
+    can initialize a Bank Account (FAILED - 1)
+    initializes with a name (FAILED - 2)
+    always initializes with balance of 1000 (FAILED - 3)
+    always initializes with a status of 'open' (FAILED - 4)
+    can't change its name
+  #deposit
+    can deposit money into its account (FAILED - 5)
+  #display_balance
+    can display its balance (FAILED - 6)
+  #valid?
+    is valid with an open status and a balance greater than 0 (FAILED - 7)
+  #close_account
+    can close its account (FAILED - 8)
+=end
+require 'pry'
 
+class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+  # binding.pry
+   new_balance =  @balance += amount
+   new_balance
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    if @balance > 0 && status == "open"
+      true
+    else
+      false
+    end
+  end
+    
+  def close_account
+     @status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+=begin
+  Transfer
+  initialize
+    can initialize a Transfer (FAILED - 9)
+    initializes with a sender (FAILED - 10)
+    initializes with a receiver (FAILED - 11)
+    always initializes with a status of 'pending' (FAILED - 12)
+    initializes with a transfer amount (FAILED - 13)
+  #valid?
+    can check that both accounts are valid (FAILED - 14)
+    calls on the sender and receiver's #valid? methods (FAILED - 15)
+  #execute_transaction
+    can execute a successful transaction between two accounts (FAILED - 16)
+    each transfer can only happen once (FAILED - 17)
+    rejects a transfer if the sender doesn't have a valid account (FAILED - 18)
+  #reverse_transfer
+    can reverse a transfer between two accounts (FAILED - 19)
+    it can only reverse executed transfers (FAILED - 20)
+=end
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, status)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+  
+  def valid?
+    
+  end
+
+  def execute_transaction
+  end
+  
+  def reverse_transfer(account)
+  end
+
 end
+
