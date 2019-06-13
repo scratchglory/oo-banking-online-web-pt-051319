145be819107c533ce8e185680f15b6a23c9fb4f8
Adella <adellakenn@gmail.com>
2019-06-13 16:57:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -45,18 +45,10 @@ class Transfer
 # rejects tranfser if the sender doesn't have a valid account
 def execute_transaction
     # binding.pry
-    # if sender.balance > amount && status == "pending"
-    #   sender.balance -= amount
-    #   receiver.balance += amount
-    #   status = "complete"
-    # else
-    #   self.status = "rejected"
-    # "Transaction rejected. Please check your account balance."
-    # end
-      if valid? && sender.balance > amount && self.status == "pending"
+    if sender.balance > amount && status == "pending"
       sender.balance -= amount
       receiver.balance += amount
-      self.status = "complete"
+      status = "complete"
     else
        self.status = "rejected"
     "Transaction rejected. Please check your account balance."
