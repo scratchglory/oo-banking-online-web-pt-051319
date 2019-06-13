f302f797ecaabe3f17fd58abdff9fca635af7303
Adella <adellakenn@gmail.com>
2019-06-13 16:52:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -50,7 +50,7 @@ def execute_transaction
       receiver.balance += amount
       status = "complete"
     else
-      status = "rejected"
+      self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
     end
